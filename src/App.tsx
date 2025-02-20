03980c475f458414602fc9eae7dee915e2a57224
BarheimCoder <barheim.coder@outlook.com>
2025-02-20 22:56:43 +0000

    Set basic click logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import Button from './components/Atoms/Button/Button';
 
 function App() {
+  const [showResult, setShowResult] = useState<boolean>(false);
+
+  const handleVoteClick = () => {
+    setShowResult(true);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="container text-center py-8 px-4 rounded-lg bg-white/20 md:w-1/3">
@@ -8,19 +15,21 @@ function App() {
           Who is going to win the F1 race? 🏎️🏁
         </h1>
         <div className="flex flex-col gap-4 my-4">
-          <Button type="button" result="48%">
+          <Button type="button" result="48%" showResult={showResult}>
             Verstappen
           </Button>
-          <Button type="button" result="20%">
+          <Button type="button" result="20%" showResult={showResult}>
             Norris
           </Button>
-          <Button type="button" result="68%">
+          <Button type="button" result="68%" showResult={showResult}>
             Alonso
           </Button>
-          <Button type="button" result="95%">
+          <Button type="button" result="95%" showResult={showResult}>
             Hamilton
           </Button>
-          <Button type="submit">Vote</Button>
+          <Button type="submit" onClick={handleVoteClick}>
+            Vote
+          </Button>
         </div>
       </div>
     </div>
--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -1,20 +1,26 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, useState } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   result?: string;
+  showResult?: boolean;
 }
 
-function Button({ children, type, result, ...props }: ButtonProps) {
+function Button({ children, type, result, showResult, ...props }: ButtonProps) {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
   const buttonStyles: string[] = [
     'relative',
-    'border-4 border-white rounded-lg backdrop:blur-lg',
+    'border-4 border-transparent rounded-lg backdrop:blur-lg',
     'px-12 py-4',
     'font-semibold',
     'transition duration-200',
     'hover:cursor-pointer hover:border-white',
     'focus:cursor-pointer focus:border-white',
-    result
+    isSelected
+      ? 'border-yellow-500 active:border-yellow-500 focus:border-yellow-500 animate-pulse'
+      : 'border-white ',
+    showResult
       ? 'bg-no-repeat bg-left bg-gradient-to-r from-indigo-500 to-pink-500'
       : '',
     type === 'submit'
@@ -22,17 +28,23 @@ function Button({ children, type, result, ...props }: ButtonProps) {
       : 'bg-white/30 hover:bg-white focus:bg-white/40',
   ];
 
+  const handleClick = () => {
+    setIsSelected(true);
+    console.log('beep');
+  };
+
   return (
     <button
       className={buttonStyles.join(' ')}
       style={{ backgroundSize: result }}
       type={type}
+      onClick={() => handleClick()}
       {...props}
     >
       {children}
 
-      {result && (
-        <div className="absolute right-0 top-0 h-full font-bold p-2 flex items-center bg-gradient-to-l from-white/80 via-white/70 via-50% to-transparent to-100%">
+      {showResult && (
+        <div className="absolute transition duration-200 right-0 top-0 h-full font-bold p-2 flex items-center bg-gradient-to-l from-white/80 via-white/70 via-50% to-transparent to-100%">
           {result}
         </div>
       )}
