af72e88dadb9cd401b98b61449b69f48b1241a7b
BarheimCoder <barheim.coder@outlook.com>
2025-02-20 22:34:21 +0000

    Add dummy question, create results display

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,22 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="container text-center py-8 px-4 rounded-lg bg-white/20 md:w-1/3">
-        <h1>Take part in the vote!</h1>
+        <h1 className="text-4xl mb-12">
+          Who is going to win the F1 race? 🏎️🏁
+        </h1>
         <div className="flex flex-col gap-4 my-4">
-          <Button type="button">Click here</Button>
-          <Button type="button">Click here</Button>
-          <Button type="button">Click here</Button>
-          <Button type="button">Click here</Button>
+          <Button type="button" result="48%">
+            Verstappen
+          </Button>
+          <Button type="button" result="20%">
+            Norris
+          </Button>
+          <Button type="button" result="68%">
+            Alonso
+          </Button>
+          <Button type="button" result="95%">
+            Hamilton
+          </Button>
           <Button type="submit">Vote</Button>
         </div>
       </div>
--- src/components/Atoms/Button/Button.tsx
+++ src/components/Atoms/Button/Button.tsx
@@ -2,23 +2,40 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  result?: string;
 }
 
-function Button({ children, type, ...props }: ButtonProps) {
+function Button({ children, type, result, ...props }: ButtonProps) {
   const buttonStyles: string[] = [
+    'relative',
     'border-4 border-white rounded-lg backdrop:blur-lg',
-    'px-6 py-4',
+    'px-12 py-4',
+    'font-semibold',
     'transition duration-200',
     'hover:cursor-pointer hover:border-white',
     'focus:cursor-pointer focus:border-white',
+    result
+      ? 'bg-no-repeat bg-left bg-gradient-to-r from-indigo-500 to-pink-500'
+      : '',
     type === 'submit'
       ? 'bg-white hover:bg-white/40 uppercase'
       : 'bg-white/30 hover:bg-white focus:bg-white/40',
   ];
 
   return (
-    <button className={buttonStyles.join(' ')} type={type} {...props}>
+    <button
+      className={buttonStyles.join(' ')}
+      style={{ backgroundSize: result }}
+      type={type}
+      {...props}
+    >
       {children}
+
+      {result && (
+        <div className="absolute right-0 top-0 h-full font-bold p-2 flex items-center bg-gradient-to-l from-white/80 via-white/70 via-50% to-transparent to-100%">
+          {result}
+        </div>
+      )}
     </button>
   );
 }
